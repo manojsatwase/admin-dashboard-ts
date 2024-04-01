05c6df3569cdae98a91265c781ffa53cb756d3ea
manoj_s <apple@manojsatwase.local>
2024-04-01 11:30:24 +0530

    --amend

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-const Home = () => {
+const Home: React.FC = () => {
   return (
-    <div style={styles}>
-      <button style={styles.button}><Link style={styles.linkTag} to='/admin/dashboard'>admin dashboard</Link></button>
+    <div style={styles.container}>
+      <button style={styles.button}>
+        <Link style={styles.linkTag} to="/admin/dashboard">
+          admin dashboard
+        </Link>
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
 
-const styles = {
-    height:"100vh",
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
-    button:{
-        background:"#202020",
-        padding:"1rem 2rem",
-        borderRadius:"8px"
-    },
-    linkTag:{
-        color:"white",
-    }
-}
+const styles: Record<string, React.CSSProperties> = {
+  container: {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    background: "#202020",
+    padding: "1rem 2rem",
+    borderRadius: "8px",
+  },
+  linkTag: {
+    color: "white",
+    textTransform: "uppercase",
+    textDecoration: "none", 
+  },
+};
